fe35b0a422481cc041350a88504c864424c7c2bf
Robert Guerra <robertjguerra@yahoo.com>
2024-06-26 21:45:09 -0700

    added view components for notifications page and explore page Integrated view components into MainTabView

---
+++ TikTokTutorial/Core/Notifications/NotificationsView.swift
@@ -0,0 +1,30 @@
+//
+//  NotificationView.swift
+//  TikTokTutorial
+//
+//  Created by Bobby Guerra on 6/26/24.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView() {
+                LazyVStack(spacing: 24) {
+                    ForEach(0 ..< 10) { notification in
+                        NotificationCell()
+                    }
+                }
+                
+                .padding(.top)
+            }
+            .navigationTitle("Notifications")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+#Preview {
+    NotificationView()
+}
---
+++ TikTokTutorial/Core/Search/UserCell.swift
@@ -0,0 +1,18 @@
+//
+//  UserCell.swift
+//  TikTokTutorial
+//
+//  Created by Bobby Guerra on 6/26/24.
+//
+
+import SwiftUI
+
+struct UserCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserCell()
+}
