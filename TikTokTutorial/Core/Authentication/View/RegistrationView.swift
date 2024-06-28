293c8029ab3ac10f6ecabe2694e76f977d783335
Robert Guerra <robertjguerra@yahoo.com>
2024-06-28 11:27:13 -0700

    added groundwork for login and registration view need to find app logo that is 1024 x 1024

---
+++ TikTokTutorial/Core/Authentication/View/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  TikTokTutorial
+//
+//  Created by Bobby Guerra on 6/28/24.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
